2feb6915dc7b6ca4d20bdbf4d78798a3b733408a
p22yxi <p22yxi@fairfieldprep.org>
2022-03-17 10:04:23 -0400

    lab14-1 and lab14-2

---
+++ lab_14-1.py
@@ -0,0 +1,22 @@
+#Yongdong Xi
+import turtle
+
+window = turtle.Screen()
+window.setup(1000, 1000)
+window.screensize(1000, 1000)
+window.title("Lab 1")
+
+
+Jason = turtle.Turtle()
+
+Jason.pendown()
+Jason.forward(250)
+Jason.right(90)
+Jason.forward(100)
+Jason.right(90)
+Jason.forward(250)
+Jason.right(90)
+Jason.forward(100)
+Jason.right(90)
+
+window.mainloop()
---
+++ lab_14-2.py
@@ -0,0 +1,18 @@
+# Yongdong Xi
+import turtle
+
+window = turtle.Screen()
+window.setup(500,500)
+window.screensize(500, 500)
+window.title('Lab 2')
+
+Peter = turtle.Turtle()
+Peter.pendown()
+Peter.right(60)
+Peter.forward(100)
+Peter.right(120)
+Peter.forward(100)
+Peter.right(120)
+Peter.forward(100)
+
+window.mainloop()
